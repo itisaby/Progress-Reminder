e9d8c53e1c0aaa35f4ba7906bc8318c25985d14c
arnab-2001 <arnabbibhuti01@gmail.com>
2021-07-15 11:54:23 +0530

    Insertion Sort added

---
+++ Arrays/Sorting/Insertion_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void display(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d \n", a[i]);
+    }
+    printf("\n");
+}
+
+void InsertionSort(int *a, int n)
+{
+    //Loop for passes
+    int k, j;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        //Loop for each pass
+        k = a[i];
+        j = i - 1;
+        while (a[j] > k && j >= 0)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = k;
+    }
+}
+
+int main()
+{
+    int a[5] = {1, 3, 2, 11, 7};
+    display(a, 5); // Printing array
+    InsertionSort(a, 5);
+    display(a, 5); // Printing array
+    return 0;
+}
