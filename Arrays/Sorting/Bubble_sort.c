d7bd77bfae8b42b69f133e94a13a36f755f4fb03
arnab-2001 <arnabbibhuti01@gmail.com>
2021-07-15 10:53:12 +0530

    July 14 work updated

---
+++ Arrays/Sorting/Bubble_sort.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+void display(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d \n", a[i]);
+    }
+    printf("\n");
+}
+
+// BubbleSort Function
+void BubbleSort(int a[], int n)
+{
+    int temp;
+    for (int i = 0; i < n - 1; i++)
+    {                                          
+        // for number of passes
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+// The time complexity of this algorithm is O(n^2)
+
+void BubbleSortOptimised(int a[], int n)
+{
+    int temp;
+    int isSorted = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        printf("%d, no. of passes \n", i + 1); // for number of passes
+        isSorted = 1;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                isSorted = 0;
+            }
+            if (isSorted)
+            {
+                return;
+            }
+        }
+    }
+}
+
+int main()
+{
+    // int a[6] = {7, 11, 9, 2, 17, 4};
+    int a[6] = {1, 2, 3, 4, 5, 6};
+    display(a, 6);             //Printing array before sorting
+    BubbleSort(a, 6);          // Function to sort the array
+    BubbleSortOptimised(a, 6); // Function to sort the array in optimised way
+    display(a, 6);             // Printing array after sorting
+
+    return 0;
+}
