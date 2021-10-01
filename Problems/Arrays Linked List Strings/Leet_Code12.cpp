b9a6b609e4d92dd712edd804ba0f9e3fd6844a41
itisdb <dhriteshbhagat@gmail.com>
2021-10-02 00:14:33 +0530

    Update Leet_Code12.cpp

--- Problems/Arrays Linked List Strings/Leet_Code12.cpp
+++ Problems/Arrays Linked List Strings/Leet_Code12.cpp
@@ -1,72 +1,34 @@
 #include<iostream>
 using namespace std;
 //   Definition for singly-linked list.
- struct ListNode
+  struct ListNode
   {
       int val;
       ListNode *next;
-      ListNode(int x) : val(x), next(NULL) {}
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
 
   class Solution
   {
   public:
-      int length(ListNode *head)
+      ListNode *removeNthFromEnd(ListNode *head, int n)
       {
-          int ct = 0;
-          while (head->next)
+          ListNode DummyNode(-1);
+          DummyNode.next = head;
+          ListNode *slow = &DummyNode;
+          ListNode *fast = &DummyNode;
+          for (int i = 0; i < n; i++)
           {
-              ct++;
-              head = head->next;
+              fast = fast->next;
           }
-          return ct;
-      }
-      ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
-      {
-          if (!headA)
+          while (fast->next != NULL)
           {
-              return headA;
+              fast = fast->next;
+              slow = slow->next;
           }
-          if (!headB)
-          {
-              return headB;
-          }
-          int l1 = length(headA);
-          int l2 = length(headB);
-          int l;
-          ListNode *temp1;
-          ListNode *temp2;
-          if (l1 > l2)
-          {
-              l = l1 - l2;
-              temp1 = headA;
-              temp2 = headB;
-          }
-          else
-          {
-              l = l2 - l1;
-              temp1 = headB;
-              temp2 = headA;
-          }
-          while (l)
-          {
-              temp1 = temp1->next;
-              if (temp1 == NULL)
-              {
-                  return NULL;
-              }
-              l--;
-          }
-          while (temp1 && temp2)
-          {
-              if (temp1 == temp2)
-              {
-                  return temp1;
-              }
-              temp1 = temp1->next;
-              temp2 = temp2->next;
-          }
-
-          return NULL;
+          slow->next = slow->next->next;
+          return DummyNode.next;
       }
   };
